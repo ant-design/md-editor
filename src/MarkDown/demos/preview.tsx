1d6a38f8fa387dd977ff346ce650f2204d5176e0
期贤 <qixian.cs@antgroup.com>
2024-05-11 11:10:54 +0800

    修复数据问题

--- src/MarkDown/demos/preview.tsx
+++ src/MarkDown/demos/preview.tsx
@@ -1,8 +1,5 @@
-﻿import {
-  MarkDownEditor,
-  NodeToSchemaType,
-} from '@chenshuai2144/md-to-json-schema';
-import { Card, Space } from 'antd';
+﻿import { NodeToSchemaType } from '@chenshuai2144/md-to-json-schema';
+import { Card, Input, Space } from 'antd';
 import { MessageRender } from './Render';
 
 //@ts-ignore
@@ -12,34 +9,49 @@ import { EditCard } from './EditCard';
 import { InvertTransition } from './Transition';
 import './air.css';
 
-const defaultValue = `# 财务数据分析报告
+const defaultValue = `<!-- {"MarkdownType": "report", "id": "8", "section_ids": " [15, 16, 17] "} -->
 
-## 蚂蚁科技集团股份有限公司财务指标概览
+# 腾讯研究报告
 
-以下是**蚂蚁科技集团股份有限公司**在**2017年12月31日**的一些关键财务指标：
+<!-- {"MarkdownType": "section", "id": "15" } -->
 
-<!-- {"chartType": "bar", "x":"描述", "y":"值"} -->
-| 财务指标 | 描述 | 值 | 是否展示 | 数据时间 | 季报时间 |
-| --- | --- | --- | --- | --- | --- |
-| accountreceivable | 应收账款/总资产 | 0.03059 | 是 | 2017-12-31 | 201712 |
-| administrationexpense_rate | 管理费用率 | 0.12105 | 是 | 2017-12-31 | 201712 |
-| basiceps | 基本每股收益 | 0.46 | 是 | 2017-12-31 | 201712 |
-| dividendps | 每股股利 | - | 是 | 2017-12-31 | 201712 |
-| financialexpense_rate | 财务费用率 | 0.00276 | 是 | 2017-12-31 | 201712 |
-| fix_cons_rate | （固定资产+在建工程）/总资产 | 0.02958 | 是 | 2017-12-31 | 201712 |
-| goodwill_rate | 商誉/总资产 | 0.01217 | 是 | 2017-12-31 | 201712 |
-| growth_accountspayable_growth_rate | 应付款增长率 | - | 是 | 2017-12-31 | 201712 |
-| growth_asset_retention_rate | 资产留存率 | 0.07259 | 是 | 2017-12-31 | 201712 |
-| growth_gross_profit_margin_growth_rate | 毛利率增长 | - | 是 | 2017-12-31 | 201712 |
+## 创始人
 
-通过上述表格，我们可以对蚂蚁科技集团股份有限公司的一些核心财务指标做初步了解。本报告主要展示出这些指标在特定财务周期的快照，并过滤只展示了公司认为有必要显示的指标。
+腾讯，全称深圳市腾讯计算机系统有限公司，是由五位创始人共同创立的，他们是马化腾、张志东、许晨晔、陈一丹和曾李青。 以下是关于这些创始人的详细信息： 马化腾 马化腾，1971 年 10 月 29 日出生于广东省东方县（现海南省东方市）八所港，广东汕头人，汉族，无党派人士。他毕业于深圳大学电子工程系计算机专业。马化腾是腾讯科技（深圳）有限公司的创始人、董事会主席、首席执行官，并曾是中华人民共和国第十二、十三届全国人民代表大会代表 。马化腾在 1998 年 11 月 11 日与合伙人共同注册成立了腾讯，并在 2004 年 6 月 16 日带领腾讯在香港联合交易所有限公司主板上市。 张志东 张志东，马化腾的同学，被称为 QQ 之父。他的计算机技术非常出色，曾是深圳大学最拔尖的学生之一。张志东在腾讯担任 CTO，并在 2014 年 9 月离职，转任腾讯公司终身荣誉顾问及腾讯学院荣誉院长等职位 。
 
-**注释**：
-- "-" 表示该项财务指标的数据未提供。
-- 所有的数值都是根据公司提交的财务数据进行统计的结果。
+<!-- {"MarkdownType": "section", "id": "16" } -->
 
-以上数据仅供参考，具体分析可能需要更深入细化的财经知识和背景信息。`;
+## 主要业务
 
+腾讯的主要业务可以分为以下几个核心领域： 增值服务：这一部分主要包括游戏和会员服务。腾讯是全球最大的游戏公司之一，提供多款流行的电子游戏。 网络广告：腾讯通过其广泛的社交平台和媒体资源，提供多样化的广告服务。这包括微信、QQ、腾讯视频等平台上的广告。 金融科技及企业服务：这包括微信支付、QQ 钱包等支付解决方案，以及腾讯云等企业服务。这些服务支持商业支付、财富管理、信贷服务等。 社交和通信平台：腾讯提供包括 QQ、微信在内的多个社交和通信平台，这些平台连接了全球超过 10 亿的用户。 数字内容：包括腾讯视频、腾讯音乐、腾讯新闻等，腾讯在内容领域提供丰富的视频、音乐、新闻等数字内容服务。 这些业务领域共同构成了腾讯的主要收入来源，并且相互支持，共同推动了腾讯的持续增长和业务创新。
+
+<!-- {"MarkdownType": "section", "id": "17" } -->
+
+## 竞争对手
+
+腾讯的竞争对手主要包括以下几家公司：  
+阿里巴巴：阿里巴巴是中国最大的电商公司，也是全球最大的电商公司之一。阿里巴巴通过其多个平台，包括淘宝、天猫、支付宝等，提供广泛的电商和金融服务。 百度：百度是中国最大的搜索引擎公司，也是中国最大的人工智能公司之一。百度通过其搜索引擎、百度地图、百度翻译等产品，提供搜索、地图、翻译等服务。 美团：美团是中国最大的外卖和本地生活服务公司，也是全球最大的外卖公司之一。美团通过其美团外卖、美团点评等平台，提供外卖、酒店、旅游等服务。 京东：京东是中国最大的综合电商公司之一，也是全球最大的综合电商公司之一。京东通过其京东商城、京东金融等平台，提供电商、金融、物流等服务。 腾讯的竞争对手主要集中在电商、搜索、外卖等领域，这些公司与腾讯在多个领域有竞争关系，但也有合作关系。腾讯通过不断创新和合作，保持了在中国互联网行业的领先地位。
+
+
+### 数据
+<!-- {"MarkdownType": "section", "id": "17","submitText":"nihao" } -->
+\`\`\`schema
+        [
+
+            { "title": "分析逻辑1", "valueType": "checkCard", "extra":"查看代码", "fieldProps": {"options":["🍎 Apple", "🍐 Pear", "🍊 Orange"]}, "width": "l" },
+            { "title": "分析模块2", "valueType": "checkCard", "extra":"查看代码", "fieldProps": {"options":[{"title":"Fruit","value":"Fruit","children":[{"title":"🍎 Apple","value":"apple"},{"title":"🍐 Pear","value":"pear"},{"title":"🍊 Orange","value":"orange"}]}]}, "width": "l" },
+        ]
+\`\`\`
+
+
+### 列表表单
+<!-- {"MarkdownType": "section", "id": "18","submitText":"send" } -->
+\`\`\`schema
+        [
+          {"title":"列表","valueType":"formList","dataIndex":"list","initialValue":[{"state":"all","title":"标题"}],"colProps":{"xs":24,"sm":12},"columns":[{"valueType":"group","columns":[{"title":"标题","dataIndex":"title","width":"md","formItemProps":{"rules":[{"required":true,"message":"此项为必填项"}]},"colProps":{"xs":24,"sm":12}}]}]}
+        ]
+\`\`\`
+`;
 export default () => {
   const [value, setValue] = useState(defaultValue);
 
@@ -67,11 +79,12 @@ export default () => {
             },
           }}
         >
-          <MarkDownEditor
-            preview="preview"
-            height="100vh"
+          <Input.TextArea
+            style={{
+              minHeight: '100vh',
+            }}
             value={value}
-            onChange={(e) => setValue(e || '')}
+            onChange={(e) => setValue(e.target.value || '')}
           />
         </Card>
         <Card
