d1703ed0d5d8cfa6324e0a05f2f03fbdcf078015
222 <113026578+222chaos@users.noreply.github.com>
2024-10-06 21:04:07 +0800

    fix: 修复同时出现多个tableattr的bug (#6)

--- src/MarkdownEditor/editor/elements/table.tsx
+++ src/MarkdownEditor/editor/elements/table.tsx
@@ -1,6 +1,16 @@
 import classNames from 'classnames';
-import React, { useCallback, useMemo } from 'react';
+import { observer } from 'mobx-react-lite';
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+import { Editor, NodeEntry, Path } from 'slate';
+import { ReactEditor } from 'slate-react';
 import { RenderElementProps } from 'slate-react/dist/components/editable';
+import { TableCellNode, TableNode } from '../../el';
 import { useSelStatus } from '../../hooks/editor';
 import { useEditorStore } from '../store';
 import { DragHandle } from '../tools/DragHandle';
@@ -14,21 +24,21 @@ export function TableCell(props: RenderElementProps) {
     store.openTableMenus(e, head);
   }, []);
   const isLatest = useMemo(() => {
-    if (store?.editor?.children.length === 0) return false;
-    if (!store?.editorProps?.typewriter) return false;
+    if (store.editor.children.length === 0) return false;
+    if (!store.editorProps.typewriter) return false;
 
     return store.isLatestNode(props.element);
   }, [
-    store?.editor?.children.at?.(path.at(0)!),
-    store?.editor?.children.at?.(path.at(0)! + 1),
-    store?.editorProps?.typewriter,
+    store.editor.children.at?.(path.at(0)!),
+    store.editor.children.at?.(path.at(0)! + 1),
+    store.editorProps.typewriter,
   ]);
   return React.useMemo(() => {
     return props.element.title ? (
       <th
         {...props.attributes}
         className={classNames({
-          typewriter: isLatest && store?.editorProps?.typewriter,
+          typewriter: isLatest && store.editorProps.typewriter,
         })}
         style={{ textAlign: props.element.align }}
         data-be={'th'}
@@ -42,7 +52,7 @@ export function TableCell(props: RenderElementProps) {
         style={{ textAlign: props.element.align }}
         data-be={'td'}
         className={classNames('group', {
-          typewriter: isLatest && store?.editorProps?.typewriter,
+          typewriter: isLatest && store.editorProps.typewriter,
         })}
         onContextMenu={(e) => {
           context(e);
@@ -54,8 +64,95 @@ export function TableCell(props: RenderElementProps) {
   }, [props.element, isLatest, props.element.children, store.refreshHighlight]);
 }
 
-export const Table = (props: RenderElementProps) => {
+export const Table = observer((props: RenderElementProps) => {
   const store = useEditorStore();
+
+  const [state, setState] = useState({
+    visible: false,
+    top: 0,
+    left: 0,
+    width: 0,
+    scaleOpen: false,
+    enterScale: false,
+    align: '' as string | undefined,
+    rows: 0,
+    cols: 0,
+    selectCols: 0,
+    selectRows: 0,
+  });
+
+  const tableRef = React.useRef<NodeEntry<TableNode>>();
+  const tableCellRef = useRef<NodeEntry<TableCellNode>>();
+
+  const resize = useCallback(() => {
+    const table = tableRef.current;
+    if (!table) return;
+    setTimeout(() => {
+      try {
+        const dom = ReactEditor.toDOMNode(
+          store.editor,
+          table[0],
+        ) as HTMLElement;
+        if (dom) {
+          setState((prev) => ({
+            ...prev,
+            rows: table[0].children.length,
+            cols: table[0].children[0].children.length,
+          }));
+        }
+      } catch (e) {}
+    }, 16);
+  }, [setState, store.editor]);
+  const handleClickOutside = useCallback(
+    (event: MouseEvent) => {
+      const tableEl = tableRef.current
+        ? ReactEditor.toDOMNode(store.editor, tableRef.current[0])
+        : null;
+      if (tableEl && !tableEl.contains(event.target as Node)) {
+        setState((prev) => ({
+          ...prev,
+          visible: false,
+        }));
+      }
+    },
+    [store.editor],
+  );
+
+  const handleClickTable = useCallback(() => {
+    const el = store.tableCellNode;
+    if (el) {
+      tableCellRef.current = el;
+      try {
+        const dom = ReactEditor.toDOMNode(store.editor, el[0]) as HTMLElement;
+        let top = dom.offsetTop;
+        let left = dom.offsetLeft;
+        setState((prev) => ({
+          ...prev,
+          top: top - 24 + 3,
+          left,
+          visible: true,
+        }));
+      } catch (error) {
+        console.log(error);
+      }
+      setState((prev) => ({
+        ...prev,
+        align: el[0].align,
+      }));
+      const table = Editor.node(store.editor, Path.parent(Path.parent(el[1])));
+      if (table && table[0] !== tableRef.current?.[0]) {
+        tableRef.current = table;
+        resize();
+      }
+    }
+  }, [store.tableCellNode, store.editor, setState]);
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [handleClickOutside]);
 
   return useMemo(() => {
     return (
@@ -64,27 +161,37 @@ export const Table = (props: RenderElementProps) => {
         {...props.attributes}
         data-be={'table'}
         onDragStart={store.dragStart}
+        onClick={handleClickTable}
+        style={{
+          maxWidth: '100%',
+          overflow: 'visible',
+          ...(store.editor.children.length === 1
+            ? {}
+            : {
+                border: '1px solid #e8e8e8',
+                borderRadius: 16,
+              }),
+        }}
       >
-        <TableAttr />
+        {state.visible && (
+          <TableAttr
+            state={state}
+            setState={setState}
+            tableRef={tableRef}
+            tableCellRef={tableCellRef}
+          />
+        )}
         <DragHandle />
-        <div
-          style={{
-            maxWidth: '100%',
-            overflow: 'auto',
-            position: 'relative',
-            ...(store?.editor?.children.length === 1
-              ? {}
-              : {
-                  border: '1px solid #e8e8e8',
-                  borderRadius: 16,
-                }),
-          }}
-        >
-          <table>
-            <tbody>{props.children}</tbody>
-          </table>
-        </div>
+        <table>
+          <tbody>{props.children}</tbody>
+        </table>
       </div>
     );
-  }, [props.element.children]);
-};
+  }, [
+    props.element.children,
+    state,
+    setState,
+    store.dragStart,
+    handleClickTable,
+  ]);
+});
