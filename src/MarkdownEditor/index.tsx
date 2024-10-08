3c1a68f7cf403de75cd382cfa4186bb210f6fae9
chenshuai2144 <qixian.cs@antgroup.com>
2024-10-08 15:48:55 +0800

    fix: 移除只读模式下的最小宽度限制

--- src/MarkdownEditor/index.tsx
+++ src/MarkdownEditor/index.tsx
@@ -270,7 +270,6 @@ export const MarkdownEditor: React.FC<MarkdownEditorProps> = (props) => {
         })}
         style={{
           width: width || '400px',
-          minWidth: readonly ? '200px' : '400px',
           height: height || 'auto',
           display: 'flex',
           flexDirection: 'column',
