56c0bc3da21d731c95691886c6c3cf06c5965410
qixian.cs <qixian.cs@antgroup.com>
2024-12-20 15:26:25 +0800

    移除tryCatchCallback中的editor参数，简化错误处理逻辑

--- src/MarkdownEditor/editor/plugins/catchError.ts
+++ src/MarkdownEditor/editor/plugins/catchError.ts
@@ -1,18 +1,18 @@
 import { Editor } from 'slate';
 const tryCatchCallback =
-  (editorFunc: any, editor: Editor) =>
+  (editorFunc: any) =>
   (...editorFuncArgs: any) => {
     try {
       return editorFunc(...editorFuncArgs);
     } catch (error) {
       console.error(error);
-      editor.undo();
+      // editor.undo();
     }
   };
 export const withErrorReporting = (editor: any): Editor => {
   Object.entries(editor).forEach(([key, value]) => {
     if (typeof value === 'function') {
-      editor[key] = tryCatchCallback(value, editor);
+      editor[key] = tryCatchCallback(value);
     }
   });
 
