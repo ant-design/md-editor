57d0dc8b8d158645e081a37f9a6b05f968a779be
qixian.cs <qixian.cs@antgroup.com>
2025-01-15 16:22:16 +0800

    feat: 为评论列表组件添加动画效果，优化样式和布局以提升用户体验

--- src/MarkdownEditor/editor/components/CommentList/index.tsx
+++ src/MarkdownEditor/editor/components/CommentList/index.tsx
@@ -10,9 +10,35 @@ import {
   MarkdownEditorInstance,
   MarkdownEditorProps,
 } from '../../../index';
+
 import { EditorStoreContext, useEditorStore } from '../../store';
 import { useStyle } from './style';
 
+const navVariants = {
+  open: {
+    transition: { staggerChildren: 0.07, delayChildren: 0.2 },
+  },
+  closed: {
+    transition: { staggerChildren: 0.05, staggerDirection: -1 },
+  },
+};
+
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 },
+    },
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 },
+    },
+  },
+};
 export const CommentList: React.FC<{
   commentList: CommentDataType[];
   comment: MarkdownEditorProps['comment'];
@@ -51,92 +77,112 @@ export const CommentList: React.FC<{
             }}
           />
         </div>
-        {props.commentList?.map((item, index) => {
-          return (
-            <div key={index} className={classNames(`${baseCls}-item`, hashId)}>
-              <div className={classNames(`${baseCls}-item-header`, hashId)}>
-                <div
-                  className={classNames(`${baseCls}-item-header-title`, hashId)}
-                >
-                  <span
-                    className={classNames(
-                      `${baseCls}-item-header-name`,
-                      hashId,
-                    )}
-                  >
-                    <Avatar src={item.user?.avatar} size={14}>
-                      {item.user?.name
-                        ?.split(' ')
-                        ?.map((item) => item?.split('').at(0)?.toUpperCase())
-                        ?.join('') || ''}
-                    </Avatar>
-                    {item.user?.name}
-                  </span>
-                  <span
-                    className={classNames(
-                      `${baseCls}-item-header-time`,
-                      hashId,
-                    )}
-                  >
-                    {dayjs(item.time).format('YYYY-MM-DD HH:mm:ss')}
-                  </span>
-                </div>
-                {readonly ? null : (
+        <motion.div
+          variants={navVariants}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 8,
+          }}
+          initial="closed"
+          whileInView="open"
+        >
+          {props.commentList?.map((item, index) => {
+            return (
+              <motion.div
+                key={index}
+                whileHover={{ scale: 1.04 }}
+                whileTap={{ scale: 0.95 }}
+                variants={itemVariants}
+                className={classNames(`${baseCls}-item`, hashId)}
+              >
+                <div className={classNames(`${baseCls}-item-header`, hashId)}>
                   <div
                     className={classNames(
-                      `${baseCls}-item-header-action`,
+                      `${baseCls}-item-header-title`,
                       hashId,
                     )}
                   >
-                    <Popconfirm
-                      title={
-                        props.comment?.deleteConfirmText ||
-                        'Are you sure to delete this comment?'
-                      }
-                      onConfirm={async (e) => {
-                        e?.stopPropagation();
-                        e?.preventDefault();
-                        try {
-                          await props.comment?.onDelete?.(item.id, item);
-                          // 更新时间戳,触发一下dom的rerender，不然不给我更新
-                          Transforms.setNodes(
-                            props.instance.store?.editor,
-                            {
-                              updateTimestamp: Date.now(),
-                            },
-                            {
-                              at: item.path,
-                            },
-                          );
-                        } catch (error) {}
-                      }}
+                    <span
+                      className={classNames(
+                        `${baseCls}-item-header-name`,
+                        hashId,
+                      )}
                     >
+                      <Avatar src={item.user?.avatar} size={14}>
+                        {item.user?.name
+                          ?.split(' ')
+                          ?.map((item) => item?.split('').at(0)?.toUpperCase())
+                          ?.join('') || ''}
+                      </Avatar>
+                      {item.user?.name}
+                    </span>
+                    <span
+                      className={classNames(
+                        `${baseCls}-item-header-time`,
+                        hashId,
+                      )}
+                    >
+                      {dayjs(item.time).format('YYYY-MM-DD HH:mm:ss')}
+                    </span>
+                  </div>
+                  {readonly ? null : (
+                    <div
+                      className={classNames(
+                        `${baseCls}-item-header-action`,
+                        hashId,
+                      )}
+                    >
+                      <Popconfirm
+                        title={
+                          props.comment?.deleteConfirmText ||
+                          'Are you sure to delete this comment?'
+                        }
+                        onConfirm={async (e) => {
+                          e?.stopPropagation();
+                          e?.preventDefault();
+                          try {
+                            await props.comment?.onDelete?.(item.id, item);
+                            // 更新时间戳,触发一下dom的rerender，不然不给我更新
+                            Transforms.setNodes(
+                              props.instance.store?.editor,
+                              {
+                                updateTimestamp: Date.now(),
+                              },
+                              {
+                                at: item.path,
+                              },
+                            );
+                          } catch (error) {}
+                        }}
+                      >
+                        <span
+                          className={classNames(
+                            `${baseCls}-item-header-action-item`,
+                            hashId,
+                          )}
+                        >
+                          <DeleteFilled />
+                        </span>
+                      </Popconfirm>
                       <span
                         className={classNames(
                           `${baseCls}-item-header-action-item`,
                           hashId,
                         )}
                       >
-                        <DeleteFilled />
+                        <EditOutlined />
                       </span>
-                    </Popconfirm>
-                    <span
-                      className={classNames(
-                        `${baseCls}-item-header-action-item`,
-                        hashId,
-                      )}
-                    >
-                      <EditOutlined />
-                    </span>
-                  </div>
-                )}
-              </div>
-              <div className={classNames(`${baseCls}-item-content`, hashId)}>
-                {item.content}
-              </div>
-            </div>
-          );
-        })}
+                    </div>
+                  )}
+                </div>
+                <div className={classNames(`${baseCls}-item-content`, hashId)}>
+                  {item.content}
+                </div>
+              </motion.div>
+            );
+          })}
+        </motion.div>
       </motion.div>
     </>,
   );
