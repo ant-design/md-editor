b8cf63918aa3bbac918f5833b52c517886372257
chenshuai2144 <qixian.cs@outlook.com>
2025-02-17 01:48:16 +0800

    feat: 禁用 LinkCard 组件的拖拽功能

--- src/MarkdownEditor/editor/elements/LinkCard/index.tsx
+++ src/MarkdownEditor/editor/elements/LinkCard/index.tsx
@@ -20,7 +20,7 @@ export function LinkCard({
     updateTime: string;
   }>
 >) {
-  const { store, markdownEditorRef, readonly } = useEditorStore();
+  const { store, markdownEditorRef } = useEditorStore();
   const context = useContext(ConfigProvider.ConfigContext);
   const baseCls = context.getPrefixCls('md-editor-link-card');
   const { wrapSSR, hashId } = useStyle(baseCls);
@@ -30,8 +30,7 @@ export function LinkCard({
       <div
         className={classNames(baseCls, 'ant-md-editor-drag-el', hashId)}
         data-be="link-card"
-        onDragStart={(e) => store.dragStart(e)}
-        draggable={readonly ? false : true}
+        draggable={false}
         onContextMenu={(e) => {
           e.stopPropagation();
         }}
