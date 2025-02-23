7311eb354a9afca3dce41e787c90a446da266552
chenshuai2144 <qixian.cs@outlook.com>
2025-02-23 21:05:34 +0800

    refactor: 移除冗余代码，优化链接插入功能的逻辑和性能

--- src/MarkdownEditor/editor/tools/InsertLink.tsx
+++ src/MarkdownEditor/editor/tools/InsertLink.tsx
@@ -1,11 +1,9 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
 import { DeleteOutlined } from '@ant-design/icons';
 import { Input, InputRef, Modal, Tooltip } from 'antd';
-import isHotkey from 'is-hotkey';
 import { runInAction } from 'mobx';
 import { observer } from 'mobx-react';
 import React, { useCallback, useRef } from 'react';
-
 import { Selection, Text, Transforms } from 'slate';
 import { IEditor } from '../..';
 import { useSubject } from '../../hooks/subscribe';
@@ -16,8 +14,6 @@ import { isLink, parsePath } from '../utils/path';
 
 type DocItem = IEditor & { path: string; parentPath?: string };
 
-const width = 370;
-
 /**
  * 链接的配置面板
  */
@@ -25,13 +21,9 @@ export const InsertLink = observer(() => {
   const { store, markdownEditorRef } = useEditorStore();
   const selRef = useRef<Selection>();
   const inputRef = useRef<InputRef>(null);
-  const scrollRef = useRef<HTMLDivElement>(null);
 
   const [state, setState] = useGetSetState({
     open: false,
-    left: 0,
-    y: 0,
-    mode: 'top',
     inputKeyword: '',
     oldUrl: '',
     index: 0,
@@ -59,110 +51,12 @@ export const InsertLink = observer(() => {
     }
   }, []);
 
-  const keydown = React.useCallback((e: KeyboardEvent) => {
-    if (isHotkey('esc', e)) {
-      close(state().oldUrl);
-    }
-    if (isHotkey('tab', e) && !isLink(state().inputKeyword)) {
-      if (state().filterAnchors.length) {
-        const target = state().filterAnchors[state().index];
-        if (target) {
-          e.preventDefault();
-          setState({ inputKeyword: target.item.path + target.value });
-        }
-      } else {
-        const target = state().filterDocs[state().index];
-        if (target) {
-          e.preventDefault();
-          setState({ inputKeyword: target.path });
-        }
-      }
-    }
-    if (isHotkey('up', e)) {
-      e.preventDefault();
-      if (
-        (state().filterDocs.length || state().filterAnchors.length) &&
-        state().index > 0
-      ) {
-        setState({
-          index: state().index - 1,
-        });
-      }
-    }
-    if (isHotkey('down', e)) {
-      e.preventDefault();
-      if (state().anchors.length) {
-        if (state().index < state().filterAnchors.length - 1) {
-          setState({
-            index: state().index + 1,
-          });
-        }
-      } else if (state().index < state().filterDocs.length - 1) {
-        setState({
-          index: state().index + 1,
-        });
-      }
-    }
-    if (isHotkey('enter', e)) {
-      e.preventDefault();
-      if (isLink(state().inputKeyword)) {
-        close(state().inputKeyword);
-      } else {
-        if (state().anchors.length) {
-          const target = state().filterAnchors[state().index];
-          if (target) {
-            setPath(target.item.path + target.value);
-          }
-        } else {
-          const target = state().filterDocs[state().index];
-          if (target) {
-            setPath(target.path);
-          } else {
-            close(state().inputKeyword);
-          }
-        }
-      }
-    }
-    const target = scrollRef.current?.children?.[
-      state()?.index
-    ] as HTMLDivElement;
-    if (target) {
-      const { scrollTop, clientHeight } = scrollRef.current!;
-      if (target.offsetTop > scrollTop + clientHeight - 50) {
-        scrollRef.current!.scroll({
-          top: target.offsetTop,
-        });
-      }
-      if (target.offsetTop < scrollTop) {
-        scrollRef.current!.scroll({
-          top: target.offsetTop - 150,
-        });
-      }
-    }
-  }, []);
-
   useSubject(store.openInsertLink$, (sel) => {
     if (store.domRect) {
       selRef.current = sel;
       store.container!.parentElement?.addEventListener('wheel', prevent, {
         passive: false,
       });
-      const mode =
-        window.innerHeight - store.domRect.top - store.domRect.height < 230
-          ? 'bottom'
-          : 'top';
-      let y =
-        mode === 'bottom'
-          ? window.innerHeight - store.domRect.top + 5
-          : store.domRect.top + store.domRect.height + 5;
-      let left = store.domRect.x;
-      left = left - (width - store.domRect.width) / 2;
-      if (left > window.innerWidth - width)
-        left = window.innerWidth - width - 4;
-      if (left < 4) left = 4;
-      if (left + width > window.innerWidth - 4) {
-        left = window.innerWidth - 4 - width;
-      }
 
       const url = EditorUtils.getUrl(markdownEditorRef.current);
       let path = url;
@@ -171,10 +65,7 @@ export const InsertLink = observer(() => {
       }
       const parse = parsePath(path);
       setState({
-        left,
-        y: y,
         oldUrl: url || '',
-        mode,
         open: true,
         filterDocs: [],
         docs: [],
@@ -190,7 +81,6 @@ export const InsertLink = observer(() => {
       setTimeout(() => {
         inputRef.current?.focus();
       }, 16);
-      window.addEventListener('keydown', keydown);
     }
   });
 
@@ -203,7 +93,6 @@ export const InsertLink = observer(() => {
       { url },
       { match: Text.isText, split: true },
     );
-    window.removeEventListener('keydown', keydown);
     runInAction(() => {
       if (typeof window === 'undefined') return;
       if (typeof window.matchMedia === 'undefined') return;
