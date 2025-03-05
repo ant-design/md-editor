9801146abd52aecbe0ba5a4edba125d9aee6f247
qixian.cs <qixian.cs@antgroup.com>
2025-03-05 11:30:16 +0800

    feat: 添加编辑器工具方法以清除标记、删除内容和重置编辑器

--- src/MarkdownEditor/editor/elements/index.tsx
+++ src/MarkdownEditor/editor/elements/index.tsx
@@ -273,7 +273,6 @@ export const MLeaf = (
           selectFormat();
         }
       }}
-      contentEditable={leaf.fnc || leaf.identifier ? false : undefined}
       data-fnc={leaf.fnc || leaf.identifier ? 'fnc' : undefined}
       data-fnd={leaf.fnd ? 'fnd' : undefined}
       data-comment={leaf.comment ? 'comment' : undefined}
