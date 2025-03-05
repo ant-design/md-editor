e4a3fe4d8c26d05e8df7b4afba24f0bbf97b03a3
qixian.cs <qixian.cs@antgroup.com>
2025-03-05 14:28:31 +0800

    feat: 优化MarkdownEditor导出，添加解析器函数

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 export * from './MarkdownEditor';
 export * from './MarkdownEditor/editor/components/index';
+export { parserMarkdownToSlateNode } from './MarkdownEditor/editor/parser/parserMarkdownToSlateNode';
+export {
+  parserSlateNodeToMarkdown,
+  parserSlateNodeToMarkdown as schemaToMarkdown,
+} from './MarkdownEditor/editor/parser/parserSlateNodeToMarkdown';
 export * from './MarkdownEditor/editor/Preview/index';
 export { schemaToHeading } from './MarkdownEditor/editor/tools/Leading';
 export * from './MarkdownEditor/editor/utils';
 export * from './MarkdownEditor/editor/utils/docx/index';
-
-export { parserMarkdownToSlateNode } from './MarkdownEditor/editor/parser/parserMarkdownToSlateNode';
-export { parserSlateNodeToMarkdown } from './MarkdownEditor/editor/parser/parserSlateNodeToMarkdown';
