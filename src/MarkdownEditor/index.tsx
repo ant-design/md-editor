9606d871317d9483559e767693b48ba863396a5f
chenshuai2144 <qixian.cs@antgroup.com>
2025-03-26 13:18:55 +0800

    移除MarkdownEditor组件中的调试日志

--- src/MarkdownEditor/index.tsx
+++ src/MarkdownEditor/index.tsx
@@ -4,7 +4,6 @@ import { BaseMarkdownEditor, MarkdownEditorProps } from './BaseMarkdownEditor';
 export * from './BaseMarkdownEditor';
 
 export const MarkdownEditor: React.FC<MarkdownEditorProps> = (props) => {
-  console.log('MarkdownEditor', props);
   return (
     <BaseMarkdownEditor
       {...props}
