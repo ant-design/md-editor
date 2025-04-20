fc5503c4e01641175ebbf897d8060aae10a611f4
chenshuai2144 <qixian.cs@outlook.com>
2025-04-20 11:22:13 +0800

    refactor: 移除不必要的 console.log 调试信息，简化代码

--- src/MarkdownEditor/editor/Editor.tsx
+++ src/MarkdownEditor/editor/Editor.tsx
@@ -521,6 +521,7 @@ export const SlateMarkdownEditor = observer(
         Transforms.insertText(markdownEditorRef.current, text, {
           at: selection!,
         });
+        return;
       }
 
       if (hasEditableTarget(markdownEditorRef.current, event.target)) {
@@ -609,7 +610,7 @@ export const SlateMarkdownEditor = observer(
 
     const plugins = useContext(PluginContext);
 
-    const elementRenderElement = useRefFunction((props: RenderElementProps) => {
+    const elementRenderElement = (props: RenderElementProps) => {
       const defaultDom = (
         <ErrorBoundary
           fallbackRender={() => {
@@ -630,9 +631,9 @@ export const SlateMarkdownEditor = observer(
       if (props.element.type === 'table-row') return defaultDom;
 
       return eleItemRender(props, defaultDom) as React.ReactElement;
-    });
+    };
 
-    const renderMarkdownLeaf = useRefFunction((props: any) => {
+    const renderMarkdownLeaf = (props: any) => {
       return (
         <MLeaf
           {...props}
@@ -643,9 +644,9 @@ export const SlateMarkdownEditor = observer(
           tagInputProps={editorProps.tagInputProps}
         />
       );
-    });
+    };
 
-    const decorateFn = useRefFunction((e: any) => {
+    const decorateFn = (e: any) => {
       const decorateList: any[] | undefined = high(e) || [];
       if (!editorProps?.comment) return decorateList;
       if (editorProps?.comment?.enable === false) return decorateList;
@@ -717,7 +718,7 @@ export const SlateMarkdownEditor = observer(
         console.log('error', error);
         return decorateList;
       }
-    });
+    };
 
     const handleSelectionChange = useDebounceFn(async () => {
       if (store.focus) {
