d545b9aaeb49863e1d7a1d8decc2d700f4bbf32b
qixian.cs <qixian.cs@outlook.com>
2025-06-20 10:52:04 +0800

    fix: update project title and name to 'markdown-editor'

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,11 +2,11 @@ import { defineConfig  } from 'dumi';
 
 export default defineConfig({
   outputPath: 'docs-dist',
-  title: 'md-editor',
+  title: 'markdown-editor',
   mfsu: false,
   themeConfig: {
     logo: 'https://mdn.alipayobjects.com/huamei_ptjqan/afts/img/A*WWGySab93QoAAAAAAAAAAAAADkN6AQ/original',
-    name: 'md-editor',
+    name: 'markdown-editor',
   },
   favicons: [
     'https://mdn.alipayobjects.com/huamei_ptjqan/afts/img/A*WWGySab93QoAAAAAAAAAAAAADkN6AQ/original',
