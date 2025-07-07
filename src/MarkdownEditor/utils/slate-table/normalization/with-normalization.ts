6ba60fcef35b9054cf67ff8404ba1548b032e8b2
qixian.cs <qixian.cs@antgroup.com>
2025-07-07 17:12:19 +0800

    优化代码格式，统一引号风格并修复部分语法问题

--- src/MarkdownEditor/utils/slate-table/normalization/with-normalization.ts
+++ src/MarkdownEditor/utils/slate-table/normalization/with-normalization.ts
@@ -1,15 +1,15 @@
-import { Editor } from "slate";
-import { WithTableOptions } from "../options";
-import { normalizeAttributes } from "./normalize-attributes";
-import { normalizeContent } from "./normalize-content";
-import { normalizeSections } from "./normalize-sections";
-import { normalizeTable } from "./normalize-table";
-import { normalizeTd } from "./normalize-td";
-import { normalizeTr } from "./normalize-tr";
+/* eslint-disable no-param-reassign */
+import { Editor } from 'slate';
+import { WithTableOptions } from '../options';
+import { normalizeAttributes } from './normalize-attributes';
+import { normalizeContent } from './normalize-content';
+import { normalizeSections } from './normalize-sections';
+import { normalizeTable } from './normalize-table';
+import { normalizeTr } from './normalize-tr';
 
 export function withNormalization<T extends Editor>(
   editor: T,
-  options: WithTableOptions
+  options: WithTableOptions,
 ): T {
   if (!options.withNormalization) {
     return editor;
@@ -18,8 +18,8 @@ export function withNormalization<T extends Editor>(
   editor = normalizeAttributes(editor, options);
   editor = normalizeContent(editor, options);
   editor = normalizeSections(editor, options);
-  editor = normalizeTable(editor, options);
-  editor = normalizeTd(editor, options);
+  editor = normalizeTable(editor);
+  // editor = normalizeTd(editor, options);
   editor = normalizeTr(editor, options);
 
   return editor;
