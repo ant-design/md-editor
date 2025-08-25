c3e307e338182c5bdc6c983ce73eff554973f52e
dingtianxiu <295434665@qq.com>
2025-08-25 15:49:42 +0800

    新增插槽功能，允许在历史列表之前插入自定义内容，更新HistoryProps接口以支持该功能。

--- src/History/index.tsx
+++ src/History/index.tsx
@@ -108,6 +108,9 @@ export const History: React.FC<HistoryProps> = (props) => {
           />
         )}
 
+        {!!props.slots?.beforeHistoryList &&
+          props.slots.beforeHistoryList(filteredList)}
+
         <GroupMenu
           selectedKeys={[props.sessionId]}
           inlineIndent={20}
