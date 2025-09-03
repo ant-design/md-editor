72131b9eaba6d155fc6556002c337b8bfb48b34f
qixian <qixian.cs@antgroup.com>
2025-09-03 16:58:58 +0800

    refactor: Remove deprecated clipPath attributes from SVG icons for cleaner code

--- src/ToolUseBar/ToolUseBarThink.tsx
+++ src/ToolUseBar/ToolUseBarThink.tsx
@@ -19,7 +19,7 @@ function ExpandDownIcon(props: React.SVGProps<SVGSVGElement>) {
           <rect width={14} height={14} rx={0} />
         </clipPath>
       </defs>
-      <g clipPath="url(#a)">
+      <g>
         <path
           d="M9.504 12.08L7 9.574l-2.504 2.504a.584.584 0 01-.825-.825l2.917-2.916a.583.583 0 01.824 0l2.917 2.916a.583.583 0 11-.825.825zM3.5 2.332c0 .155.061.303.17.413l2.918 2.916a.583.583 0 00.824 0l2.917-2.916a.583.583 0 10-.825-.825L7 4.425 4.496 1.921a.583.583 0 00-.996.412z"
           fillRule="evenodd"
@@ -44,7 +44,7 @@ function ExpandIcon(props: React.SVGProps<SVGSVGElement>) {
           <rect width={14} height={14} rx={0} />
         </clipPath>
       </defs>
-      <g clipPath="url(#a)">
+      <g>
         <path
           d="M9.504 5.662L7 3.158 4.496 5.662a.584.584 0 11-.825-.824L6.588 1.92a.583.583 0 01.824 0l2.917 2.917a.583.583 0 01-.825.824zM7 10.842L4.496 8.338a.583.583 0 10-.825.824l2.917 2.917a.583.583 0 00.824 0l2.917-2.916a.584.584 0 10-.825-.825L7 10.842z"
           fillRule="evenodd"
