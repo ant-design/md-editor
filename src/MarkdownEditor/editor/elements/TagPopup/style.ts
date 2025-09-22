f7865a6470046efcb4a5066ee9e4d210c17dd52d
qixian <qixian.cs@antgroup.com>
2025-09-22 18:19:32 +0800

    fix: 调整 TagPopup 样式，优化字体和透明度设置

--- src/MarkdownEditor/editor/elements/TagPopup/style.ts
+++ src/MarkdownEditor/editor/elements/TagPopup/style.ts
@@ -8,12 +8,11 @@ const genStyle: GenerateStyle<ChatTokenType> = (token) => {
   return {
     [token.componentCls]: {
       position: 'relative',
-      margin: '0 4px',
       cursor: 'pointer',
       padding: '0px 4px',
-      fontSize: '0.9em',
       display: 'inline-flex',
       lineHeight: '1.5',
+      font: 'var(--font-text-body-base)',
       color: 'var(--color-primary-text-secondary)',
       borderRadius: 'var(--radius-control-sm)',
       background: 'var(--color-primary-bg-tip)',
@@ -22,7 +21,6 @@ const genStyle: GenerateStyle<ChatTokenType> = (token) => {
         '&:not(.tag-popup-input-composition).empty::before': {
           color: 'var(--color-primary-text-disabled)',
           content: 'attr(title)',
-          opacity: 0.5,
           userSelect: 'none',
           position: 'absolute',
           left: '4px',
@@ -36,7 +34,6 @@ const genStyle: GenerateStyle<ChatTokenType> = (token) => {
         '&.empty::after': {
           content: 'attr(title)',
           opacity: 0,
-          height: '0.5px',
           overflow: 'hidden',
           userSelect: 'none',
         },
