039a6b0bede31d619f5958786f3e3acc269a8a16
qixian <qixian.cs@antgroup.com>
2025-09-24 14:45:42 +0800

    feat: refactor DownOutlined icon into a separate file and update imports in TagPopup and ToolUseBar components

--- src/ToolUseBar/ToolUseBarThink.tsx
+++ src/ToolUseBar/ToolUseBarThink.tsx
@@ -2,86 +2,10 @@ import { ConfigProvider } from 'antd';
 import classNamesFn from 'classnames';
 import { useMergedState } from 'rc-util';
 import React, { useContext } from 'react';
+import { ExpandDownIcon, ExpandIcon } from '../icons';
+import { DownOutlined } from '../icons/DownOutlined';
 import { ThinkIcon } from '../icons/ThinkIcon';
 import { useStyle } from './thinkStyle';
-
-function DownOutlined(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      width="1em"
-      height="1em"
-      viewBox="0 0 14 14"
-      {...props}
-    >
-      <defs>
-        <clipPath id="a">
-          <rect width={14} height={14} rx={0} />
-        </clipPath>
-      </defs>
-      <g clipPath="url(#a)">
-        <path
-          d="M10.088 13.83L7 10.741l-3.087 3.087a.583.583 0 11-.825-.825l3.5-3.5a.583.583 0 01.824 0l3.5 3.5a.583.583 0 11-.824.825z"
-          fillRule="evenodd"
-          fill="currentColor"
-          transform="matrix(1 0 0 -1 0 18.667)"
-        />
-      </g>
-    </svg>
-  );
-}
-
-function ExpandDownIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      width={14}
-      height={14}
-      viewBox="0 0 14 14"
-      {...props}
-    >
-      <defs>
-        <clipPath id="a">
-          <rect width={14} height={14} rx={0} />
-        </clipPath>
-      </defs>
-      <g>
-        <path
-          d="M9.504 12.08L7 9.574l-2.504 2.504a.584.584 0 01-.825-.825l2.917-2.916a.583.583 0 01.824 0l2.917 2.916a.583.583 0 11-.825.825zM3.5 2.332c0 .155.061.303.17.413l2.918 2.916a.583.583 0 00.824 0l2.917-2.916a.583.583 0 10-.825-.825L7 4.425 4.496 1.921a.583.583 0 00-.996.412z"
-          fillRule="evenodd"
-          fill="currentColor"
-        />
-      </g>
-    </svg>
-  );
-}
-function ExpandIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      width={14}
-      height={14}
-      viewBox="0 0 14 14"
-      {...props}
-    >
-      <defs>
-        <clipPath id="a">
-          <rect width={14} height={14} rx={0} />
-        </clipPath>
-      </defs>
-      <g>
-        <path
-          d="M9.504 5.662L7 3.158 4.496 5.662a.584.584 0 11-.825-.824L6.588 1.92a.583.583 0 01.824 0l2.917 2.917a.583.583 0 01-.825.824zM7 10.842L4.496 8.338a.583.583 0 10-.825.824l2.917 2.917a.583.583 0 00.824 0l2.917-2.916a.584.584 0 10-.825-.825L7 10.842z"
-          fillRule="evenodd"
-          fill="currentColor"
-        />
-      </g>
-    </svg>
-  );
-}
 
 export interface ToolUseBarThinkProps {
   toolName: React.ReactNode;
