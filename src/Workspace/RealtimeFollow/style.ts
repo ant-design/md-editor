6733c891dd291c3557fbba042a33987e94977707
lu <luai.zc@digital-engine.com>
2025-09-25 18:00:32 +0800

    feat: 接入工作空间-实时跟随代码编辑器onetoken

--- src/Workspace/RealtimeFollow/style.ts
+++ src/Workspace/RealtimeFollow/style.ts
@@ -15,8 +15,33 @@ const genStyle: GenerateStyle<ChatTokenType> = (token) => {
         paddingTop: '8px',
         paddingBottom: '8px',
 
+        // 返回按钮
+        [`&-back-button`]: {
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '32px',
+          height: '32px',
+          border: 'none',
+          background: 'none',
+          cursor: 'pointer',
+          color: '#767e8b',
+          transition: 'all 0.2s ease',
+          borderRadius: 'var(--radius-control-base)',
+          backdropFilter: 'blur(20px)',
+
+          '&:hover': {
+            background: '#f0f0f0',
+          },
+        },
+
+        // 返回图标
+        [`&-back-icon`]: {
+          fontSize: '16px',
+        },
+
         [`&-with-border`]: {
-          borderBottom: '1px solid var(--color-gray-border-light)',
+          borderBottom: '1px solid rgba(20, 22, 28, 0.07)',
         },
 
         [`&-left`]: {
@@ -29,32 +54,40 @@ const genStyle: GenerateStyle<ChatTokenType> = (token) => {
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
-          width: '40px',
-          height: '40px',
           borderRadius: '6px',
 
           [`&--html`]: {
+            width: '16px',
+            height: '16px',
             background: '#e0f9ff',
           },
 
           [`&--default`]: {
+            width: '40px',
+            height: '40px',
             background: '#eef1f6',
           },
         },
 
         [`&-content`]: {
           display: 'flex',
-          flexDirection: 'column',
+          alignItems: 'center',
+          gap: '8px',
         },
 
         [`&-title`]: {
-          fontSize: 'var(--font-size-base)',
-          fontWeight: 500,
+          display: 'flex',
+          fontFamily: 'AlibabaPuHuiTi',
+          fontSize: '14px',
+          fontWeight: '500',
           lineHeight: '22px',
+          letterSpacing: 'normal',
           color: 'var(--color-gray-text-default)',
         },
 
         [`&-subtitle`]: {
+          fontSize: 'var(--font-size-sm)',
+          fontWeight: 'normal',
           color: 'var(--color-gray-text-secondary)',
           font: 'var(--font-text-body-sm)',
           letterSpacing: 'var(--letter-spacing-body-sm, normal)',
@@ -63,13 +96,46 @@ const genStyle: GenerateStyle<ChatTokenType> = (token) => {
         [`&-right`]: {
           display: 'flex',
           alignItems: 'center',
+
+          '.ant-segmented': {
+            /* 圆角-控件-base */
+            borderRadius: 'var(--radius-control-base)',
+            /* gray/gray-控件填充-按下.激活 */
+            background: 'var(--color-gray-control-fill-active)',
+          },
+
+          '.ant-segmented-item-selected': {
+            borderRadius: 'var(--radius-control-base)',
+            /* gray/gray-背景-白色卡片 */
+            /* 样式描述：contrast */
+            background: 'var(--color-gray-bg-card-white)',
+            /* 投影-控件-base */
+            boxShadow: 'var(--shadow-control-base)',
+          },
         },
 
         // Segmented 右侧额外内容容器
         [`&-segmented-right`]: {
           display: 'flex',
           alignItems: 'center',
+          justifyContent: 'center',
           gap: '8px',
+         
+          'button' :{
+            background: 'none',
+            cursor: 'pointer',
+            color: '#CC545D6D',
+            transition: 'all 0.2s ease',
+            backdropFilter: 'blur(20px)',
+            borderRadius: 'var(--radius-control-sm)',
+            boxSizing: 'border-box',
+            border: '0px solid rgba(0, 30, 75, 0.07)',
+
+            '&:hover': {
+              background: '#f0f0f0',
+            },
+          }
+    
         },
       },
 
@@ -117,6 +183,34 @@ const genStyle: GenerateStyle<ChatTokenType> = (token) => {
         width: '100%',
         height: '100%',
         overflow: 'auto', // 添加滚动支持
+
+        '.ace-container': {
+          marginTop: 0,
+          borderRadius: 'unset',
+        },
+
+        '.ant-workspace-html-preview-content .ace-container': {
+          border: 'none',
+          borderRadius: 'unset',
+          boxShadow: 'none!important',
+        },
+
+        '.ace_gutter': {
+          color: 'var(--color-gray-text-light)',
+          font: 'var(--font-text-code-base)',
+          letterSpacing: 'var(--letter-spacing-code-base, normal)',
+          background: 'var(--color-gray-bg-card-white)',
+        },
+
+        '.ace_gutter-cell': {
+          paddingLeft: 12,
+        },
+
+        '.ace-tm .ace_content': {
+          fontFamily: 'Roboto Mono',
+          fontSize: '12px',
+          letterSpacing: 'var(--letter-spacing-code-base, normal)',
+        },
       },
 
       [`&-overlay`]: {
@@ -145,31 +239,6 @@ const genStyle: GenerateStyle<ChatTokenType> = (token) => {
         minHeight: '240px',
         padding: '24px',
         textAlign: 'center',
-      },
-
-      // ==================== Overrides ====================
-      [`&-segmented`]: {
-        color: 'var(--color-gray-text-secondary)',
-        fontSize: '13px',
-        background: 'var(--color-gray-control-fill-active)',
-        borderRadius: '8px',
-
-        '.ant-segmented-item-selected': {
-          color: 'var(--color-gray-text-default)',
-          background: 'var(--color-gray-bg-card-white)',
-          borderRadius: '8px',
-          boxShadow: 'var(--shadow-control-base)',
-
-          '.ant-workspace-tab-title': {
-            font: 'var(--font-text-h6-base)',
-            letterSpacing: 'var(--letter-spacing-h6-base, normal)',
-          },
-        },
-
-        '.ant-segmented-item:hover:not(.ant-segmented-item-selected):not(.ant-segmented-item-disabled)':
-          {
-            color: 'var(--color-gray-text-default)',
-          },
       },
     },
   };
