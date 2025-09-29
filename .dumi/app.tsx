6a57e58262e2dcabb7166b34b034f15d2d1871d6
qixian <qixian.cs@antgroup.com>
2025-09-29 16:49:34 +0800

    chore: 更新文档和样式，优化按钮组件的使用示例

--- .dumi/app.tsx
+++ .dumi/app.tsx
@@ -1,9 +1,11 @@
+import { globalThemeToken, useCSSVariables } from '@ant-design/theme-token';
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/es/locale/zh_CN';
 import React from 'react';
 import './reset-ant.css';
 
 export function rootContainer(container: any) {
+  useCSSVariables('ThemeExample', globalThemeToken);
   return React.createElement(
     ConfigProvider,
     {
