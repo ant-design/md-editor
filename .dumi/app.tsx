5c97ceab84533a67128925663bad4e467f36f898
qixian <qixian.cs@antgroup.com>
2025-09-29 17:02:42 +0800

    fix: 修复 insertCSSVariables 调用，添加缺失的参数 'app'

--- .dumi/app.tsx
+++ .dumi/app.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 import './reset-ant.css';
 
 export function rootContainer(container: any) {
-  insertCSSVariables('ThemeExample', globalThemeToken);
+  insertCSSVariables('ThemeExample', globalThemeToken, 'app');
   return React.createElement(
     ConfigProvider,
     {
