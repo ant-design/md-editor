f8dfe424f466596a6ac94e38114c2c9cd5fd08bd
qixian <qixian.cs@antgroup.com>
2025-09-29 17:00:17 +0800

    fix: 更新 theme-token 依赖版本，修复 CSS 变量插入功能

--- .dumi/app.tsx
+++ .dumi/app.tsx
@@ -1,11 +1,11 @@
-import { globalThemeToken, useCSSVariables } from '@ant-design/theme-token';
+import { globalThemeToken, insertCSSVariables } from '@ant-design/theme-token';
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/es/locale/zh_CN';
 import React from 'react';
 import './reset-ant.css';
 
 export function rootContainer(container: any) {
-  useCSSVariables('ThemeExample', globalThemeToken);
+  insertCSSVariables('ThemeExample', globalThemeToken);
   return React.createElement(
     ConfigProvider,
     {
