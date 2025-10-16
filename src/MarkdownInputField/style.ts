c7e62c01a280ce186a2e1b5e5ddd06878c87eaa4
圣贞 <yuyanxi.yyx@digital-engine.com>
2025-10-16 17:51:17 +0800

    fix: 样式优化

--- src/MarkdownInputField/style.ts
+++ src/MarkdownInputField/style.ts
@@ -1,4 +1,4 @@
-import { Keyframes } from '@ant-design/cssinjs';
+﻿import { Keyframes } from '@ant-design/cssinjs';
 import {
   ChatTokenType,
   GenerateStyle,
@@ -213,7 +213,7 @@ const genStyle: GenerateStyle<ChatTokenType> = (token) => {
       gap: 8,
       width: '100%',
       maxWidth: '980px',
-      padding: '0 10px 0 0',
+      padding: '0',
       marginBottom: '8px',
       font: 'var(--font-text-body-base)',
       color: 'var(--color-gray-text-default)',
